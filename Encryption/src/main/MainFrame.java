9bf56bcb1947572f7510b916440de544d8cff243
84485 <84485@students.mentorschools.org>
2017-05-09 12:21:02 -0400

    resjfkad

--- Encryption/src/main/MainFrame.java
+++ Encryption/src/main/MainFrame.java
@@ -33,6 +33,7 @@ public class MainFrame extends JFrame {
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		this.setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
